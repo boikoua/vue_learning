56e995386d5d9bc987a27d8cecfe73b95046cb5e
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-11-04 20:20:42 +0200

    prepare search

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, reactive, ref, watch } from 'vue';
 import axios from 'axios';
 
 import Header from './components/Header.vue';
@@ -8,10 +8,35 @@ import Drawer from './components/Drawer.vue';
 
 const items = ref([]);
 
+const filters = reactive({
+  sortBy: '',
+  searchQuery: '',
+});
+
+const onChangeSelect = event => {
+  filters.sortBy = event.target.value;
+};
+
+const onChangeSearch = event => {
+  filters.searchQuery = event.target.value;
+};
+
 onMounted(async () => {
   try {
     const { data } = await axios.get(
       'https://1e2507bc8b8d8220.mokky.dev/items',
+    );
+
+    items.value = data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+watch(filters, async () => {
+  try {
+    const { data } = await axios.get(
+      `https://1e2507bc8b8d8220.mokky.dev/items?sortBy=${filters.sortBy}&title=*${filters.searchQuery}*`,
     );
 
     items.value = data;
@@ -31,15 +56,19 @@ onMounted(async () => {
         <h2 class="text-3xl font-bold mb-8">Усі кросівки</h2>
 
         <div class="flex gap-4">
-          <select class="py-1.5 px-3 border rounded-md outline-none">
-            <option value="">За назвою</option>
-            <option value="">За ціною (від меншої)</option>
-            <option value="">За ціною (від більшої)</option>
+          <select
+            @change="onChangeSelect"
+            class="py-1.5 px-3 border rounded-md outline-none"
+          >
+            <option value="title">За назвою</option>
+            <option value="price">За ціною (від меншої)</option>
+            <option value="-price">За ціною (від більшої)</option>
           </select>
 
           <div class="relative">
             <img class="absolute top-2 left-3" src="/search.svg" alt="Search" />
             <input
+              @change="onChangeSearch"
               class="border rounded-md py-1 pl-10 pr-4 outline-none focus:border-gray-400"
               type="text"
               placeholder="Пошук..."
