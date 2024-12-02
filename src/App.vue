70cbd62d2b2333cc2f73a15bf8a1d5fb618176e2
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-12-02 14:23:34 +0200

    start now

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,47 @@
 <script setup>
-import { onMounted, reactive, ref, watch } from 'vue';
-import axios from 'axios';
-
-import Header from './components/Header.vue';
-import CardList from './components/CardList.vue';
-import Drawer from './components/Drawer.vue';
-
-const items = ref([]);
-
-const filters = reactive({
-  sortBy: 'title',
-  searchQuery: '',
-});
-
-const onChangeSelect = event => {
-  filters.sortBy = event.target.value;
-};
-
-const onChangeSearch = event => {
-  filters.searchQuery = event.target.value;
-};
-
-const fetchItems = async () => {
-  try {
-    const params = {
-      sortBy: filters.sortBy,
-    };
-
-    if (filters.searchQuery) {
-      params.title = `*${filters.searchQuery}*`;
-    }
-
-    const { data } = await axios.get(
-      `https://1e2507bc8b8d8220.mokky.dev/items`,
-      { params },
-    );
-
-    items.value = data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-onMounted(fetchItems);
-
-watch(filters, fetchItems);
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <!-- <Drawer /> -->
-  <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl my-10">
-    <Header />
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <main class="p-10">
-      <div class="flex justify-between items-center mb-10">
-        <h2 class="text-3xl font-bold mb-8">Усі кросівки</h2>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
 
-        <div class="flex gap-4">
-          <select
-            @change="onChangeSelect"
-            class="py-1.5 px-3 border rounded-md outline-none"
-          >
-            <option value="title">За назвою</option>
-            <option value="price">За ціною (від меншої)</option>
-            <option value="-price">За ціною (від більшої)</option>
-          </select>
-
-          <div class="relative">
-            <img class="absolute top-2 left-3" src="/search.svg" alt="Search" />
-            <input
-              @input="onChangeSearch"
-              class="border rounded-md py-1 pl-10 pr-4 outline-none focus:border-gray-400"
-              type="text"
-              placeholder="Пошук..."
-            />
-          </div>
-        </div>
-      </div>
-
-      <CardList :items="items" />
-    </main>
-  </div>
+  <main>
+    <TheWelcome />
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
