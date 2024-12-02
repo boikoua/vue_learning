d8a19a1406da63aa23fe0256bccd0ed6fdae0f8b
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-12-02 20:21:50 +0200

    Header complete

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
-<script setup></script>
+<script setup>
+import Header from './components/Header.vue';
+</script>
 
-<template></template>
+<template>
+  <div
+    class="w-11/12 lg:w-4/5 m-auto bg-zinc-100 h-screen rounded-2xl shadow-xl my-4 md:my-6 lg:my-10"
+  >
+    <Header />
+  </div>
+</template>
 
 <style scoped></style>
