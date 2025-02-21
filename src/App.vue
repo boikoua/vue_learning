df8b693e74f545132cf023f6883a84798a422d5d
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-02-21 16:56:01 +0200

    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,88 @@
 <script setup>
 import { ref } from 'vue';
 
-const cssClasses = 'color bg-color border padding';
+const message = ref('Hello');
 
-const cssObject = ref({
-  done: true,
-  selected: false,
-});
+const userName = ref('');
 
-const isValid = true;
-const isDisabled = true;
+const name = ref('');
+const surname = ref('');
+const fatherName = ref('');
+
+function getUserName(userInput) {
+  if (!userInput.trim()) return;
+
+  const prepareName = userInput.split(' ');
+
+  name.value = prepareName[0];
+  surname.value = prepareName[1];
+  fatherName.value = prepareName[2];
+
+  userName.value = '';
+}
+
+const dataTextarea = ref('');
+const arrayStrings = ref([]);
+
+function stringToArray(value) {
+  if (!value.trim()) return;
+
+  arrayStrings.value = value.split(' ');
+}
+
+const checked = ref(true);
+
+const answersArray = ref([]);
+
+const isDisabled = ref(true);
 </script>
 
 <template>
-  <p :class="cssClasses">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit consequuntur enim quo
-    reiciendis nam obcaecati vero, facilis recusandae soluta nesciunt?
+  <input type="text" v-model="message" />
+  <br />
+  <p>{{ message.toUpperCase() }}</p>
+  <hr />
+
+  <input type="text" v-model="userName" />
+  <button @click="getUserName(userName)">Send</button>
+  <p>Имя: {{ name }}</p>
+  <p>Фамилия: {{ surname }}</p>
+  <p>Отчетсво: {{ fatherName }}</p>
+
+  <div>
+    <textarea name="" id="" v-model="dataTextarea"></textarea>
+    <br />
+    <br />
+    <button @click="stringToArray(dataTextarea)">Send</button>
+
+    <ul v-show="arrayStrings.length">
+      <li v-for="(item, index) in arrayStrings" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+
+  <hr />
+
+  <input type="checkbox" v-model="checked" />
+  <br />
+  <br />
+  <p v-show="checked">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi at saepe cumque dolorum aliquid
+    quibusdam eligendi exercitationem molestias similique architecto.
   </p>
 
-  <p :class="cssObject">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam cum ut ratione saepe
-    perferendis. Ratione enim optio nihil natus minus numquam animi, distinctio dignissimos neque,
-    sunt ipsa? Eaque, officiis earum.
-  </p>
+  <hr />
 
-  <button
-    @click="
-      () => {
-        cssObject.done = !cssObject.done;
-        cssObject.selected = !cssObject.selected;
-      }
-    "
-  >
-    toggle
-  </button>
+  <input type="checkbox" v-model="answersArray" value="HTML" /> HTML
+  <input type="checkbox" v-model="answersArray" value="CSS" /> CSS
+  <input type="checkbox" v-model="answersArray" value="JS" /> JS
 
-  <p :class="{ 'bg-color': isValid, padding: isDisabled }">
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum numquam incidunt aut!
-  </p>
+  <ul>
+    <li v-for="item of answersArray" :key="item">{{ item }}</li>
+  </ul>
 
-  <p
-    :style="{
-      color: 'green',
-      'background-color': 'yellow',
-      fontWeight: 'bold',
-      'font-style': 'italic',
-    }"
-  >
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, numquam quis rerum et, nihil
-    natus beatae quia facilis incidunt dolore aut pariatur animi ex! Qui harum temporibus totam
-    laborum non sequi pariatur? Illo a accusamus assumenda quaerat iusto amet minus. Debitis
-    consequuntur delectus quis a dolore, excepturi numquam quos quas?
-  </p>
+  <hr />
+  <input type="text" :disabled="isDisabled" />
+  <button @click="() => (isDisabled = !isDisabled)">toggle</button>
 </template>
 
-<style scoped>
-.color {
-  color: blue;
-}
-
-.bg-color {
-  background-color: orangered;
-}
-
-.border {
-  border: 2px solid gray;
-}
-
-.padding {
-  padding: 10px 20px;
-}
-
-.done {
-  background-color: greenyellow;
-  text-decoration: line-through;
-  transition: all 1s;
-}
-
-.selected {
-  background-color: bisque;
-  transition: all 1s;
-}
-</style>
+<style scoped></style>
