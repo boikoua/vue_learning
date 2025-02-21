ee9cd1dc511fc58b99406af7182d8a0af1d5d086
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-02-21 15:47:32 +0200

    work with css

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,84 @@
 <script setup>
 import { ref } from 'vue';
 
-const items = ref(['hello', 'my', 'car']);
+const cssClasses = 'color bg-color border padding';
 
-function addNewItem() {
-  items.value.push('i am a new item');
-}
+const cssObject = ref({
+  done: true,
+  selected: false,
+});
 
-function deleteFirstItem() {
-  items.value.shift();
-}
-
-function sortAllItems() {
-  items.value.sort((a, b) => a.localeCompare(b));
-}
-
-function reverseItems() {
-  items.value.reverse();
-}
+const isValid = true;
+const isDisabled = true;
 </script>
 
 <template>
-  <button @click="addNewItem">add a new item</button>
-  <br />
-  <br />
-  <button @click="deleteFirstItem">delete the first item</button>
-  <br />
-  <br />
-  <button @click="sortAllItems">sort all items</button>
-  <br />
-  <br />
-  <button @click="reverseItems">reverse all items</button>
+  <p :class="cssClasses">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit consequuntur enim quo
+    reiciendis nam obcaecati vero, facilis recusandae soluta nesciunt?
+  </p>
 
-  <ul>
-    <li v-for="(item, index) in items" :key="index">{{ item }}</li>
-  </ul>
+  <p :class="cssObject">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam cum ut ratione saepe
+    perferendis. Ratione enim optio nihil natus minus numquam animi, distinctio dignissimos neque,
+    sunt ipsa? Eaque, officiis earum.
+  </p>
+
+  <button
+    @click="
+      () => {
+        cssObject.done = !cssObject.done;
+        cssObject.selected = !cssObject.selected;
+      }
+    "
+  >
+    toggle
+  </button>
+
+  <p :class="{ 'bg-color': isValid, padding: isDisabled }">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum numquam incidunt aut!
+  </p>
+
+  <p
+    :style="{
+      color: 'green',
+      'background-color': 'yellow',
+      fontWeight: 'bold',
+      'font-style': 'italic',
+    }"
+  >
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, numquam quis rerum et, nihil
+    natus beatae quia facilis incidunt dolore aut pariatur animi ex! Qui harum temporibus totam
+    laborum non sequi pariatur? Illo a accusamus assumenda quaerat iusto amet minus. Debitis
+    consequuntur delectus quis a dolore, excepturi numquam quos quas?
+  </p>
 </template>
 
-<style scoped></style>
+<style scoped>
+.color {
+  color: blue;
+}
+
+.bg-color {
+  background-color: orangered;
+}
+
+.border {
+  border: 2px solid gray;
+}
+
+.padding {
+  padding: 10px 20px;
+}
+
+.done {
+  background-color: greenyellow;
+  text-decoration: line-through;
+  transition: all 1s;
+}
+
+.selected {
+  background-color: bisque;
+  transition: all 1s;
+}
+</style>
