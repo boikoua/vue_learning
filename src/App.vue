4d27f07657aa92c7356542853f1773b50e9209bd
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-02-22 10:44:11 +0200

    work with components and emits

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,47 @@
 <script setup>
 import { ref } from 'vue';
-
-const items = ref(['car']);
-const inputValue = ref('');
-
-function addNewItem() {
-  if (inputValue.value) {
-    items.value.unshift(inputValue.value);
-  }
-}
-
-function removeItem(index) {
-  items.value = items.value.filter((_, i) => i !== index);
-}
+import TheEmployee from './components/TheEmployee.vue';
 
 const users = ref([
   {
     id: 1,
-    name: 'name1',
-    surn: 'surn1',
-    isEdit: false,
+    name: 'David',
+    salary: 600,
+    age: 30,
+    langs: ['html', 'css', 'js'],
   },
   {
     id: 2,
-    name: 'name2',
-    surn: 'surn2',
-    isEdit: false,
+    name: 'John',
+    salary: 1000,
+    age: 40,
+    langs: ['html', 'css', 'js', 'ts'],
   },
   {
     id: 3,
-    name: 'name3',
-    surn: 'surn3',
-    isEdit: false,
+    name: 'Christi',
+    salary: 1500,
+    age: 50,
+    langs: ['html', 'css', 'js', 'c#'],
   },
 ]);
 
-function editFunc(user) {
-  user.isEdit = true;
-}
-
-function saveFunc(user) {
-  user.isEdit = false;
+function removeUser(id) {
+  users.value = users.value.filter((user) => user.id !== id);
 }
 </script>
 
 <template>
-  <ul>
-    <li v-for="user in users" :key="user.id">
-      <template v-if="!user.isEdit">
-        {{ user.name }}
-        {{ user.surn }}
-
-        <button @click="editFunc(user)">edit</button>
-      </template>
-
-      <template v-else>
-        <input type="text" v-model="user.name" />
-        <input type="text" v-model="user.surn" />
-
-        <button @click="saveFunc(user)">save</button>
-      </template>
-    </li>
-  </ul>
-  <hr />
-  <input type="text" v-model="inputValue" />
-  <br />
-  <br />
-  <button @click="addNewItem">Add a new item</button>
-
-  <ul v-show="items.length">
-    <li v-for="(item, index) of items" :key="index">
-      {{ item }} <button @click="removeItem(index)">Remove Item</button>
-    </li>
-  </ul>
+  <TheEmployee
+    v-for="user in users"
+    :key="user.id"
+    :id="user.id"
+    :name="user.name"
+    :salary="user.salary"
+    :age="user.age"
+    :languages="user.langs"
+    @delete="removeUser"
+  />
 </template>
 
 <style scoped></style>
---
+++ src/components/TheEmployee.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  id: Number,
+  name: String,
+  salary: Number,
+  age: Number,
+  languages: {
+    type: Array,
+  },
+});
+
+defineEmits(['delete']);
+</script>
+
+<template>
+  <h2>Hello from Employee</h2>
+  <p>Name: {{ name }}</p>
+  <p>Salary: {{ salary }}$</p>
+  <p>Age: {{ age }}</p>
+  <ul>
+    <li v-for="(lang, index) in languages" :key="index">{{ lang }}</li>
+  </ul>
+
+  <div>
+    <button @click="$emit('delete', id)">Remove user</button>
+  </div>
+  <hr />
+</template>
+
+<style scoped></style>
